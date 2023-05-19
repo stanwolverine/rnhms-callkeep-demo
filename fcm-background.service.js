cb21b26d2a53d1e93dc3616bbe04873e6f91eba6
Jatin Nagar <jatin.nagar@100ms.live>
2023-05-20 03:15:59 +0530

    fix: :bug: app crash on missing import statements

--- fcm-background.service.js
+++ fcm-background.service.js
@@ -1,3 +1,5 @@
+import messaging from '@react-native-firebase/messaging';
+
 // Register background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('FCM Message handled in the background!', JSON.stringify(remoteMessage, null, 2));
